03b376c1af1541305570b1b3b5211c89435082c4
Julia Sanchez <2509juliasanchez@gmail.com>
2024-03-04 20:07:07 -0300

    refactor: 💡 remove unused animation

--- src/components/Banner/components/Content/Content.tsx
+++ src/components/Banner/components/Content/Content.tsx
@@ -21,9 +21,6 @@ export const Content = () => {
       {
         duration: 1,
         delay: 0.5,
-        morphSVG: {
-          shape: 'M 0 0 Q 100 20 200 0 Q 300 20 400 0 Q 500 20 600 0 Q 700 20 800 0 Q 900 20 1000 0',
-        },
         ease: 'power3.inOut'
       }
     );
@@ -52,7 +49,7 @@ export const Content = () => {
     <div className="w-full h-2/5 flex justify-center items-center lg:w-1/2 lg:h-full lg:justify-end">
       <div className='flex justify-start flex-col items-start w-2/3'>
         <h1 className='text text-left text-4xl font-bold mb-1 w-full relative p-1 md:text-[4vw] md:mb-6 md:leading-none'>
-          O melhor jeito de <span ref={textRef}>gerenciar</span> seu dinheiro
+          O melhor jeito de gerenciar seu dinheiro
         </h1>
         <h2 className='text-stone-600 text text-left text-xl font-regular mb-6 w-full p-1 md:text-2xl md:mb-8'>
           Transforme sua relação com o dinheiro. Ganhe controle total sobre suas finanças com nossa plataforma intuitiva e eficiente.
